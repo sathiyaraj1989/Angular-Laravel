e797555a350561daaece90a9bcc764b57322dd8e
sathiyaraj1989 <sathiyabesh5@gmail.com>
2018-04-05 20:11:33 +0530

    Added for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './components/index';
+import { routes } from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
